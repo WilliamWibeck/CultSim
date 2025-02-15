87d940bbc85719ee25c85230a1388ac5e4f2462c
wiwi <william.wibeck@schoolsoft.se>
2025-02-15 22:56:40 +0100

    Add cult simulation with follower dynamics and turn system
    Introduce the `Cult` class to model cults with attributes like name, leader, charisma, fear, and follower count. Implement methods for recruiting and losing followers based on charisma and fear levels. Update `Main` to simulate multiple cults over 10 turns, showcasing dynamic follower changes.

--- src/main/java/org/sim/Cult.java
+++ src/main/java/org/sim/Cult.java
@@ -1,4 +1,109 @@
 package org.sim;
 
+import java.util.Random;
+
 public class Cult {
+    public String name;
+    public String leaderName;
+    public int followerCount;
+    public double leaderCharismaLevel;
+    public double fearLevel;
+    public boolean active;
+
+    public Cult (String name, String leaderName, int followerCount, double leaderCharismaLevel, double fearLevel){
+        this.name = name;
+        this.leaderName = leaderName;
+        this.followerCount = followerCount;
+        this.leaderCharismaLevel = leaderCharismaLevel;
+        this.fearLevel = fearLevel;
+        this.active = true;
+    }
+
+    public void recruitFollowers() {
+
+        Random rand = new Random();
+
+        double randomInt = rand.nextDouble() * 10;
+
+        int newFollowers = (int) Math.round(this.leaderCharismaLevel * randomInt);
+
+        if(newFollowers > this.leaderCharismaLevel){
+            this.followerCount += newFollowers;
+            if(leaderCharismaLevel < 10 && newFollowers > followerCount / 10){
+                this.fearLevel = fearLevel * 0.95;
+                this.leaderCharismaLevel = leaderCharismaLevel * 1.1;
+                System.out.println(this.leaderName + " gains the support of his followers");
+                if(this.leaderCharismaLevel >= 10) {
+                    this.leaderCharismaLevel = 10;
+                }
+
+                if(this.fearLevel < 0) {
+                    this.fearLevel = 0;
+                }
+            }
+
+            System.out.println("The cult of " + this.name + " has recruited " + newFollowers + " followers" + " Total: " + this.followerCount);
+        }else {
+            System.out.println("The cult of " + this.name + " has not grown.");
+        }
+    }
+    public void loseFollowers() {
+
+        Random rand = new Random();
+
+        double randomDouble = rand.nextDouble() * 10;
+
+        int lostFollowers = (int) Math.round((this.fearLevel * randomDouble) - (this.leaderCharismaLevel * 1.8));
+
+        if(this.followerCount > 0) {
+            if(lostFollowers > this.fearLevel){
+                this.followerCount -= lostFollowers;
+
+                if(followerCount < 0){
+                    followerCount = 0;
+                    this.active = false;
+                }
+
+                if(lostFollowers > followerCount / 20){
+                    this.fearLevel = fearLevel * 1.1;
+                    this.leaderCharismaLevel = leaderCharismaLevel * 0.95;
+                }
+                System.out.println("The cult of " + this.name + " has lost " + lostFollowers + " followers" + " Total: " + this.followerCount);
+            }
+        } else {
+            System.out.println("The cult of " + this.name + " has collapsed");
+        }
+    }
+
+    public void loseOrGainFollowers() {
+        Random rand = new Random();
+
+        if(this.fearLevel > 8 || this.leaderCharismaLevel < 5) {
+            loseFollowers();
+        }
+
+        else if(this.leaderCharismaLevel > 5 && this.fearLevel <= 7) {
+            recruitFollowers();
+        }
+
+        else {
+            int randomInt = rand.nextInt(100);
+            if(randomInt > 85){
+                loseFollowers();
+            }
+        }
+
+
+
+
+    }
+
+    public void takeTurn(){
+        if(this.active){
+        loseOrGainFollowers();
+        }
+
+    }
+
+
 }
--- src/main/java/org/sim/Main.java
+++ src/main/java/org/sim/Main.java
@@ -1,7 +1,24 @@
-package org.example;
+package org.sim;
+
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Cult scienthologyCult = new Cult("Scienthology", "Tom Cruise", 902, 8.2, 7.6 );
+        Cult churchOfFierySkies = new Cult("Church of fiery skies", "Jesus", 524, 9.5, 1.4);
+        Cult doomsdayBelievers = new Cult("Doomsday Believers", "Aubrey Plaza", 209, 4.6, 10.0);
+        Cult scarletCrusade = new Cult("The Scarlet Crusade", "Alexander Mograine", 2012, 8.8, 5.6);
+
+        ArrayList<Cult> cults = new ArrayList<>();
+
+        cults.add(scienthologyCult);
+        cults.add(churchOfFierySkies);
+        cults.add(doomsdayBelievers);
+        cults.add(scarletCrusade);
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("\n Turn: " + "[" + i + "]");
+            cults.stream().forEach(Cult::takeTurn);
+        }
     }
 }
